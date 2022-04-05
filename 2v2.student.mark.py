776038d594c65b26ebfe2502099a3acbb35b7927
dusk <minhducnguyen71.12@gmail.com>
2022-04-05 22:53:55 +0700

    .

--- 2v2.student.mark.py
+++ 2v2.student.mark.py
@@ -2,7 +2,6 @@
 class Student:
 
     students = {}
-
     def __init__(self, name, id, dob):
         self.name = name
         self.id = id
@@ -10,22 +9,29 @@ class Student:
 
     @staticmethod
     def create():
-        id=input("student id:")
         name=input("student name:")
+        id=input("student id:")
         dob=input("student dob:")
         if id in Student.students:
             print("Id existed")
             return None 
         else:
-            s=Student(id,name,dob)
+            s=Student(name,id,dob)
             Student.students[s.id]=s
             return s
         
     @staticmethod
     def show():
         for student_id in Student.students:
-            print(Student.students[student_id])
-
+            print(Student.students[student_id].name)
+            print(Student.students[student_id].dob)
+    @staticmethod
+    def find(student_id):
+        if(student_id in Student.students):
+            return Student.students[student_id]
+        else:
+            print("there's no student id")
+            return None
 class Course:
 
     courses = {}
@@ -45,28 +51,45 @@ class Course:
             course = Course(name, id)
             Course.courses[course.id] = course
             return course
+    def find(course_id):
+        if(course_id in Course.courses):
+            return Course.courses[course_id]
+        else:
+            print("there's no course id")
+            return None
+        
 
 class Mark(Course):
     
     marks = {}
-    
-    def __init__(self, course, student, id):
+    tuple=(Course.courses,Student.students)
+
+
+    def __init__(self, course, student, score):
         super().__init__(course.name, course.id)
         self.student = student
         self.id = id
+        self.score= score
     
     @staticmethod
     def create():
         student_id=input("student id:")
         course_id=input("course id:")
         score = input("Grade:")
-        Mark.marks[(student_id, course_id)] = score
+        student1=Student.find(student_id)
+        course1=Course.find(course_id)
+        newmark=Mark(course1,student1,score)
+        Mark.marks[(student_id, course_id)] = newmark
+        
+
 
     @staticmethod
-    def lookup():
-        student_id=input("student id:")
-        course_id=input("course id:")
-        print(Mark.marks[(student_id, course_id)])
+    def find(student_id,course_id):
+        if (student_id,course_id) in Mark.marks:
+            return Mark.marks[(student_id, course_id)]
+        else:
+            print("there's none")
+            return None
 
 Student.create()
 Student.show()
@@ -74,13 +97,10 @@ Student.show()
 math = Course.create()
 
 Mark.create()
-Mark.lookup()
-print(math)
+student_id=input("student id:")
+course_id=input("course id:")
+mark1=Mark.find(student_id,course_id)
+print(mark1.score)
+Student.find(student_id)
 
-students = {}
-courses = {}
-marks = {}
 
-
-id = "1"
-students[id]
